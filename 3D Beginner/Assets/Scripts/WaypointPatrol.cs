f0e3839f862f2ab1bb0db43a2023794c713f200c
SeoChangWoo <62105366+SeoChangWoo@users.noreply.github.com>
2022-09-11 23:45:05 +0900

    20220911 Update

---
+++ 3D Beginner/Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    int currentWaypointIndex; 
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+    void Update()
+    {
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1);
+        }
+    }
+}
